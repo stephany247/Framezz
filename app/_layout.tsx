36e9408fc7ade544d8b9e8a3f51a2a48d94eab6f
Stephanie Oguocha <stephanieoguocha@gmail.com>
2025-11-09 11:41:06 +0100

    chore: project setup with NativeWind, Convex backend, and Clerk auth integration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,18 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ClerkProvider tokenCache={tokenCache}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        />
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    </ClerkProvider>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Link } from "expo-router";
+import "../global.css";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+      <Link href="/(home)">
+        <Text>Sign in</Text>
+      </Link>
+    </View>
+  );
+}
