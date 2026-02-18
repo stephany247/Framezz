c024d1c29a380a2066e4e31c1280f87747e60e7c
Stephanie Oguocha <stephanieoguocha@gmail.com>
2026-02-18 16:26:58 +0100

    feat: improve create screen theming for light and dark mode

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { ScrollView, Text } from "react-native";
+import { ScrollView, Text, useColorScheme } from "react-native";
 import PostComposer from "@/components/PostComposer";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function UploadScreen() {
+  const scheme = useColorScheme();
+  // const isDark = scheme === "dark";
+
   return (
-    <SafeAreaView className="flex-1 bg-black">
-      <ScrollView contentContainerStyle={{ padding: 16 }} className="bg-black">
-        <Text className="text-2xl font-bold text-white mb-4">Create a post</Text>
+    <SafeAreaView edges={["top"]} className="flex-1 bg-white dark:bg-gray-950">
+      <ScrollView
+        contentContainerStyle={{ padding: 16 }}
+        className="bg-white dark:bg-gray-950"
+      >
         <PostComposer />
       </ScrollView>
     </SafeAreaView>
